2eb752e27d1aa1f9862ef9446e30df3dac38ffd9
Documenter.jl <documenter@juliadocs.github.io>
2021-01-25 18:15:52 +0000

    build based on 8a20045

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Breadth","category":"page"},{"location":"#Breadth","page":"Home","title":"Breadth","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Breadth]","category":"page"}]
+}
